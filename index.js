57d6807a245160651c93e84dd31e0ce678e24710
Pedro Augusto <26pedrocastro@gmail.com>
2022-08-23 23:02:31 -0300

    .env settings

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import { Octokit } from "octokit";
+import * as dotenv from 'dotenv'
 
 (async function () {
+  dotenv.config()
 
   const octokit = new Octokit({
     auth: process.env.GITHUB_AUTH
   })
 
   const res = await octokit.request('GET /user/repos', {})
-
   console.log(res)
+  console.log(process.env.GITHUB_AUTH)
+
 }());
